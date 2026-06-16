62018530710aa66e3e14044e89601ef799a28412
Pedro150708 <pedrohgf15@gmail.com>
2026-06-16 16:23:18 -0300

    d

--- site/src/components/Header.jsx
+++ site/src/components/Header.jsx
@@ -1,7 +1,35 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import "./Header.css";
 
+
 export default function Header() {
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const temaSalvo = localStorage.getItem("theme");
+
+    if (temaSalvo === "dark") {
+      setDarkMode(true);
+      document.body.classList.add("dark-mode");
+    }
+  }, []);
+
+  const toggleDarkMode = () => {
+    const novoTema = !darkMode;
+
+    setDarkMode(novoTema);
+
+    if (novoTema) {
+      document.body.classList.add("dark-mode");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.body.classList.remove("dark-mode");
+      localStorage.setItem("theme", "light");
+    }
+  };
+
   return (
     <>
       <header className="montclair-header">
@@ -13,12 +41,19 @@ export default function Header() {
             {/* Esquerda */}
             <div className="col-4 col-md-3">
               <div className="d-flex gap-3">
-                <button className="header-circle-btn">
-                  <i className="bi bi-three-dots"></i>
+                <button
+                  className="header-circle-btn"
+                  data-bs-toggle="offcanvas"
+                  data-bs-target="#menuLateral"
+                >
+                  <i className="bi bi-list"></i>
                 </button>
 
-                <button className="header-circle-btn">
-                  <i className="bi bi-moon"></i>
+                <button
+                  className="header-circle-btn"
+                  onClick={toggleDarkMode}
+                >
+                  <i className={`bi ${darkMode ? "bi-sun-fill" : "bi-moon"}`}></i>
                 </button>
               </div>
             </div>
@@ -77,15 +112,6 @@ export default function Header() {
 
         </div>
 
-
-        <nav className="header-nav">
-          <Link to="/">Início</Link>
-          <Link to="/produtos">Produtos</Link>
-          <Link to="/masculino">Masculino</Link>
-          <Link to="/feminino">Feminino</Link>
-          <Link to="/contato">Contato</Link>
-        </nav>
-
         {/* Barra de benefícios */}
         <div className="benefits-bar">
 
@@ -117,6 +143,37 @@ export default function Header() {
         </div>
 
       </header>
+      <div
+  className="offcanvas offcanvas-start"
+  tabIndex="-1"
+  id="menuLateral"
+>
+
+  <div className="offcanvas-header">
+    <h5 className="offcanvas-title">
+      Menu
+    </h5>
+
+    <button
+      type="button"
+      className="btn-close"
+      data-bs-dismiss="offcanvas"
+    ></button>
+  </div>
+
+  <div className="offcanvas-body">
+
+    <ul className="navbar-mobile">
+      <li><a href="Home">Início</a></li>
+      <li><a href="Produtos">Produtos</a></li>
+      <li><a href="">Masculino</a></li>
+      <li><a href="">Feminino</a></li>
+      <li><a href="">Contato</a></li>
+    </ul>
+
+  </div>
+
+</div>
     </>
   );
 }
